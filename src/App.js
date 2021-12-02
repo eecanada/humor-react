b93e0512d003d71fab532fda9806e0f1771c837e
Eder Canada <canada.eder@gmail.com>
2021-12-01 18:10:53 -0800

    joke

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [joke, setJoke] = useState('');
+
+  const handleClick = () => {
+    axios
+      .get(` https://cors-anywhere.herokuapp.com/https://api.yomomma.info/`)
+      .then((response) => setJoke(response.data.joke));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}> Jokes </button>
+      <h1>{joke}</h1>
     </div>
   );
-}
+};
 
 export default App;

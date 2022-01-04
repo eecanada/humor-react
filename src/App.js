5ce18e5f515571e5880d49ec9caa10cca584390b
Eder Canada <canada.eder@gmail.com>
2022-01-03 22:19:37 -0800

    tried cors, did not work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './App.css';
 
 const App = () => {
   const [joke, setJoke] = useState('');
 
-  const handleClick = () => {
+  function getData() {
     axios
-      .get(` https://cors-anywhere.herokuapp.com/https://api.yomomma.info/`)
+      .get(`https://cors-anywhere.herokuapp.com/https://yomamma-api.herokuapp.com/jokes`)
       .then((response) => setJoke(response.data.joke));
+  }
+
+  const handleClick = () => {
+    getData();
   };
 
   return (
-    <div>
+    <div className="joke">
       <button onClick={handleClick}> Jokes </button>
       <h1>{joke}</h1>
     </div>
